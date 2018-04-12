f3c6471711ad6e718c4a1cf1fe1bea79ecc04e7c
Simonetta Andrea <A.Simonetta@almaviva.it>
2018-04-12 15:45:09 +0200

    Added entry point for the current 'webpack' configuration

---
+++ index.js
@@ -0,0 +1,38 @@
+// Import pickadate
+import 'pickadate/lib/compressed/picker.js'
+import 'pickadate/lib/compressed/picker.date.js'
+import 'pickadate/lib/compressed/picker.time.js'
+
+// Import bootstrap's js
+// require('exports-loader?'
+//   .concat('Util=', 'bootstrap/js/src/util.js', '&')
+//   .concat('Alert=', 'bootstrap/js/src/alert.js', '&')
+//   .concat('Button=', 'bootstrap/js/src/button.js', '&')
+//   .concat('Carousel=', 'bootstrap/js/src/carousel.js', '&')
+//   .concat('Collapse=', 'bootstrap/js/src/collapse.js', '&')
+//   .concat('Dropdown=', 'bootstrap/js/src/dropdown.js', '&')
+//   .concat('Modal=', 'bootstrap/js/src/modal.js', '&')
+//   .concat('Tooltip=', 'bootstrap/js/src/tooltip.js', '&')
+//   .concat('Popover=', 'bootstrap/js/src/popover.js', '&')
+//   .concat('Scrollspy=', 'bootstrap/js/src/scrollspy.js', '&')
+//   .concat('Tab=', 'bootstrap/js/src/tab.js')
+// )
+
+
+// Import bootstrap-italia js
+import './src/js/plugins/cookiebar.js'
+import './src/js/plugins/image-grid.js'
+import './src/js/plugins/zoom.js'
+import './src/js/plugins/forms.js'
+import './src/js/plugins/custom-select.js'
+import './src/js/plugins/offcanvas.js'
+import './src/js/plugins/date-picker.js'
+import './src/js/plugins/back-to-top.js'
+import './src/js/plugins/forward.js'
+import './src/js/bootstrap-italia.js'
+
+// Import bootstrap-italia styles
+import './src/scss/bootstrap-italia.scss'
+
+// Import bootstrap-italia icon fonts
+import './src/icons/css/italia-icon-font.css'
